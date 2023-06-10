208e67178cbd369a708d1ab99f1247be026dc7c8
Devisreenb <devisreenb@gmail.com>
2023-06-10 13:31:52 +0530

    adding - second smallest and largest element in array

--- arrays/Problems on Array/Second smallest and Second largest element in an array.py
+++ arrays/Problems on Array/Second smallest and Second largest element in an array.py
@@ -1,12 +1,33 @@
+import sys
 class Solution :
     def SecondSmallestLargest(self,arr):
         n = len(arr)
-    
-       
-        return 0
+        secondmin = min = sys.maxsize
+        secondmax = max = -sys.maxsize-1
+        for i in range(0,n):
+            if arr[i]<min:
+                min = arr[i]
+            if arr[i]>max:
+                max = arr[i]
+        for i in range(0,n):
+            if arr[i]<secondmin and arr[i]!=min  :
+                secondmin = arr[i]
+            if arr[i]>secondmax and arr[i]!=max:
+                secondmax = arr[i]
+        return (secondmin,secondmax)
 
 output = Solution()
-print(output.SecondSmallestLargest([0,2,3,4,4,5]))
+print(output.SecondSmallestLargest([0,2,3,5,4,5]))
+
+'''
+This is optimal solution which uses for loops and finds the min and max elements intially and later looping through again finding the second smallest
+and largest element by finding minimum element which is not equal to min element and finding maximum element not equal to max element.
+Time complexity : O(N)
+
+Bruteforce :
+This approach is most suitable when no duplicates exist in the list. Here we sort the list and find arr[1],arr[n-1] elements .
+Time Complexity : O(NlogN)
+'''
 
 
         
